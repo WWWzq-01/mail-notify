0616896e1874afa3a817d28556d0021f022e66b6
WWWzq-01 <wzq20011222@outlook.com>
2024-07-07 16:11:32 +0800

    feat: separate the stdout and stderr
    Signed-off-by: WWWzq-01 <wzq20011222@outlook.com>

--- notify.sh
+++ notify.sh
@@ -3,13 +3,20 @@
 # 使用默认的收件人邮箱地址，如果没有提供则使用该默认值
 DEFAULT_EMAIL="wwwzq1222@163.com"
 
-
 # 检查是否提供了邮箱地址，如果没有则使用默认值
 EMAIL=${2:-$DEFAULT_EMAIL}
 
+# 创建临时文件用于捕获输出和错误
+OUTFILE=$(mktemp)
+ERRFILE=$(mktemp)
+
 # 执行命令并捕获输出和错误
-OUTPUT=$(eval "$1" 2>&1)
+eval "$1" >"$OUTFILE" 2>"$ERRFILE"
 STATUS=$?
+
+# 读取输出和错误内容
+OUTPUT=$(cat "$OUTFILE")
+ERROR=$(cat "$ERRFILE")
 
 # 检查命令执行状态
 if [ $STATUS -eq 0 ]; then
@@ -17,5 +24,8 @@ if [ $STATUS -eq 0 ]; then
     echo -e "The command '$1' has completed successfully.\n\nOutput:\n$OUTPUT" | mail -s "Command Completion Notification" "$EMAIL"
 else
     # 发送失败通知邮件，并附上命令的输出和错误
-    echo -e "The command '$1' has failed.\n\nOutput:\n$OUTPUT" | mail -s "Command Failure Notification" "$EMAIL"
+    echo -e "The command '$1' has failed.\n\nOutput:\n$OUTPUT\n\nError:\n$ERROR" | mail -s "Command Failure Notification" "$EMAIL"
 fi
+
+# 清理临时文件
+rm "$OUTFILE" "$ERRFILE"
