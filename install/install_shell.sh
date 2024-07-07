11468670b658f454d8dd9206d520551209487021
WWWzq-01 <wzq20011222@outlook.com>
2024-07-07 15:36:21 +0800

    refactor: extract alias code to `bash add_alias.sh`
    Signed-off-by: WWWzq-01 <wzq20011222@outlook.com>

--- install/install_shell.sh
+++ install/install_shell.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
 ln -s $PWD/../notify.sh /usr/local/bin/notify.sh
-echo "Symbolic link created successfully."
-# 获取用户的默认 shell
-user_shell=$(basename $SHELL)
 
-# 根据 shell 类型确定配置文件
-case "$user_shell" in
-    bash)
-        shell_config_file="$HOME/.bashrc"
-        ;;
-    zsh)
-        shell_config_file="$HOME/.zshrc"
-        ;;
-    ksh)
-        shell_config_file="$HOME/.kshrc"
-        ;;
-    fish)
-        shell_config_file="$HOME/.config/fish/config.fish"
-        ;;
-    *)
-        echo "Unsupported shell: $user_shell"
-        exit 1
-        ;;
-esac
-
-# 添加 alias 到配置文件中
-echo "alias notify='/usr/local/bin/notify.sh'" >> $shell_config_file
-
-# 重新加载配置文件
-source $shell_config_file
-
-echo "Alias added and configuration file reloaded successfully."
+bash add_alias.sh
