5649e11f19bd28d2a09ba126faeddf0f1193311b
WWWzq-01 <wzq20011222@outlook.com>
2024-07-07 15:45:59 +0800

    feat: add stdout&stderr to email content
    Signed-off-by: WWWzq-01 <wzq20011222@outlook.com>

--- notify.sh
+++ notify.sh
@@ -7,14 +7,15 @@ DEFAULT_EMAIL="wwwzq1222@163.com"
 # 检查是否提供了邮箱地址，如果没有则使用默认值
 EMAIL=${2:-$DEFAULT_EMAIL}
 
-# 执行命令
-eval "$1"
+# 执行命令并捕获输出和错误
+OUTPUT=$(eval "$1" 2>&1)
+STATUS=$?
 
 # 检查命令执行状态
-if [ $? -eq 0 ]; then
-    # 发送通知邮件
-    echo "The command '$1' has completed successfully." | mail -s "Command Completion Notification" "$EMAIL"
+if [ $STATUS -eq 0 ]; then
+    # 发送成功通知邮件，并附上命令的输出
+    echo -e "The command '$1' has completed successfully.\n\nOutput:\n$OUTPUT" | mail -s "Command Completion Notification" "$EMAIL"
 else
-    # 发送失败通知邮件
-    echo "The command '$1' has failed." | mail -s "Command Failure Notification" "$EMAIL"
+    # 发送失败通知邮件，并附上命令的输出和错误
+    echo -e "The command '$1' has failed.\n\nOutput:\n$OUTPUT" | mail -s "Command Failure Notification" "$EMAIL"
 fi
