0ec767abfe2ae8e23a99ee2345fc706e0240369d
WWWzq-01 <wzq20011222@outlook.com>
2024-07-07 12:27:35 +0800

    feat: add alias function
    use add_alias.sh to add alias for notify.sh to .xxsherc
    
    Signed-off-by: WWWzq-01 <wzq20011222@outlook.com>

---
+++ install/add_alias.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 获取用户的默认 shell
+user_shell=$(basename $SHELL)
+
+# 根据 shell 类型确定配置文件
+case "$user_shell" in
+    bash)
+        shell_config_file="$HOME/.bashrc"
+        ;;
+    zsh)
+        shell_config_file="$HOME/.zshrc"
+        ;;
+    ksh)
+        shell_config_file="$HOME/.kshrc"
+        ;;
+    fish)
+        shell_config_file="$HOME/.config/fish/config.fish"
+        ;;
+    *)
+        echo "Unsupported shell: $user_shell"
+        exit 1
+        ;;
+esac
+
+# 添加 alias 到配置文件中
+echo "alias notify='/usr/local/bin/notify.sh'" >> $shell_config_file
+
+# 重新加载配置文件
+source $shell_config_file
+
+echo "Alias added and configuration file reloaded successfully."
